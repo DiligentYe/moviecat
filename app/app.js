171578b554a323b55bc58c2cdd95bd06b32e818c
yyp <yyp@yypdeMacBook-Pro.local>
2017-01-12 18:51:08 +0800

    实现模块化，将三个功能用一个控制器和视图控制

--- app/app.js
+++ app/app.js
@@ -3,9 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('moviecat', [
         'ngRoute',
-        'moviecat.in_theaters',
-        'moviecat.comming_soon',
-        'moviecat.top250'
+        'moviecat.movie_list'
+
     ])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.otherwise({ redirectTo: '/in_theaters/1' });
