c42c20c4236aafc85e9c8cf1e18e576f6eea0a98
yyp <yyp@yypdeMacBook-Pro.local>
2017-01-13 09:41:07 +0800

    在自定义指令中，通过监视$location.path(),进行焦点切换，其中处理了正则匹配过程中产生的前后空格

--- app/components/auto-focus.js
+++ app/components/auto-focus.js
@@ -3,33 +3,39 @@
         .directive('autoFocus', [
             '$location',
             function($location) {
-                var path = $location.path();
-                if (!path) {
-                    path = '/in_theaters/1';
-                }
+                // var path = $location.path();
+                // if (!path) {
+                //     path = '/in_theaters/1';
+                // }
                 return {
                     restrict: 'A',
                     link: function($scope, element, attribute) {
                         // 刚加在页面时，焦点选定问题
-                        // var path = $location.path().split('/')[1];
-                        // var aLink = element.children().attr('href').split('/')[1];
 
                         // 使用正则表达是匹配字符
                         // #/in_theaters/1
-                        var aLink = element.children().attr('href');
-                        aLink = aLink.replace(/#\/(\w+)\/\d+/, '$1').replace(/(\w+)\s*/, '$1');
-                        // path = path.replace(/\/(.+?)\/\d+/, '$1')
 
-                        if (path.match(aLink)) {
-                            element.addClass('active');
-                        }
-
-                        // 给每个元素绑定点击事件，给点击的按钮添加active样式
-                        element.on('click', function() {
-                            // 排他思想
-                            element.parent().children().removeClass('active');
-                            element.addClass('active');
+                        // 将$location挂在$scope上，用于监视
+                        $scope.$location = $location;
+                        $scope.$watch('$location.path()', function(now) {
+                            console.log(now);
+                            var aLink = element.children().attr('href');
+                            aLink = aLink.replace(/#\/(\w+)\/\d+/, '$1').replace(/\s*(\w+)\s*/, '$1');
+                            console.log(aLink);
+                            if (now.match(aLink)) {
+                                element.parent().children().removeClass('active');
+                                element.addClass('active');
+                                console.log('ok');
+                            }
                         })
+
+                        // // 给每个元素绑定点击事件，给点击的按钮添加active样式
+                        // element.on('click', function() {
+                        //     // 排他思想
+                        //     element.parent().children().removeClass('active');
+                        //     element.addClass('active');
+                        // })
+
                     }
                 }
             }
